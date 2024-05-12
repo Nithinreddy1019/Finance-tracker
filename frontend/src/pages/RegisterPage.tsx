8517ff03e1ab59becc1bdab5db24d3341b0b3606
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-12 20:43:46 +0530

    page: added register page

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,10 +1,56 @@
 import { NavBar } from "../components/navbar";
+import { Socials } from "../components/socials";
+import { Button } from "../components/ui/button";
+import { CardWrapper } from "../components/ui/card-wrapper";
+import { InputBox } from "../components/ui/input-box";
 
 const RegisterPage = () => {
     return (
-        <div>
+        <div className="h-screen bg-white dark:bg-dark-backgroundone dark:text-light-background">
             <NavBar />
-            Register page
+            
+            <section className="flex items-center justify-center h-full p-2">
+
+                <CardWrapper
+                    headerName="Hola! from FT"
+                    headerCaption="Please signin to continue"
+                    backLinkDialog="Already have an account?"
+                    backLinkName="Signin"
+                    backLink="/login"
+                >
+                    <InputBox 
+                        inputLabel="Email"
+                        inputType="email"
+                        inputPlaceholder="Johndoe@gmail.com"
+                    />
+
+                    <InputBox 
+                        inputLabel="Name"
+                        inputType="text"
+                        inputPlaceholder="John doe"
+                    />
+                    
+                    <InputBox 
+                        inputLabel="Password"
+                        inputType="password"
+                        inputPlaceholder="********"
+                    />
+
+                    <Button
+                        buttonType="primary"
+                        className="my-2"
+                    >
+                        Signup
+                    </Button>
+
+                    <hr className="my-2"/>
+
+                    <Socials />
+
+                </CardWrapper>
+
+            </section>
+
         </div>
     )
 };
