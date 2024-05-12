a63d5ad63ecfce0cd229f7c3f6b6bfb4d3979fb4
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-12 17:40:49 +0530

    ui: added card wrapper

---
+++ frontend/src/components/ui/card-wrapper.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+interface CardWrapperProps {
+    headerName?: string,
+    headerCaption?: string,
+    backLinkDialog?: string,
+    backLinkName?: string,
+    backLink: string,
+    children: React.ReactNode
+}
+
+export const CardWrapper = ({
+    headerName,
+    headerCaption,
+    backLinkDialog,
+    backLinkName,
+    backLink,
+    children
+}: CardWrapperProps) => {
+    return (
+        <div className="flex flex-col items-center justify-center gap-y-4 border-2 rounded-3xl p-4 w-[350px] md:w-[450px] border-light-bordercolor dark:border-dark-bordercolor dark:bg-dark-backgroundtwo ">
+
+            <div className="flex flex-col items-center">
+                <h1 className="text-3xl font-semibold">
+                    {headerName}
+                </h1>
+                <p className="text-sm font-semibold">
+                    {headerCaption}
+                </p>
+            </div>
+
+            <div>
+                {children}
+            </div>
+
+            <div className="flex items-center justify-center">
+                <p className="text-xs">
+                    {backLinkDialog}
+                </p>
+                <p className="text-xs italic underline pl-1 hover:cursor-pointer">
+                    <Link to={backLink}>
+                        {backLinkName}
+                    </Link>
+                </p>
+            </div>
+
+        </div>
+    )
+};
