23da65f29b1cb5558733ec76e190ed8fa12ab026
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-14 17:09:52 +0530

    page: modifications

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,54 +1,30 @@
+import { LoginForm } from "../components/auth/LoginForm";
+import { Carousel } from "../components/carousel";
 import { NavBar } from "../components/navbar";
-import { Socials } from "../components/socials";
-import { Button } from "../components/ui/button";
-import { CardWrapper } from "../components/ui/card-wrapper";
-import { InputBox } from "../components/ui/input-box";
-import { LoadFramerMotion } from "../components/ui/load-framer-motion";
+
+import finance from "../assets/carouselImages/finance.svg";
+import expenses from "../assets/carouselImages/expenses.svg";
+import investments from "../assets/carouselImages/investments.svg";
 
 const LoginPage = () => {
+
+    const imagesList = [finance, expenses, investments];
+
     return (
-        <div className="h-screen bg-white dark:bg-dark-backgroundone dark:text-light-background">
+        <>
             <NavBar />
-            
-            <section className="flex items-center justify-center h-full p-2">
-
-                <LoadFramerMotion>
-                <CardWrapper
-                    headerName="Welcome back"
-                    headerCaption="Please login to continue"
-                    backLinkDialog="Don't have an account?"
-                    backLinkName="Signup"
-                    backLink="/register"
-                >
-                    <InputBox 
-                        inputLabel="Email"
-                        inputType="email"
-                        inputPlaceholder="Johndoe@gmail.com"
+            <section className="grid grid-cols-3">
+                <div className="hidden md:block md:col-span-1 ">
+                    <Carousel 
+                        imagesList={imagesList}
                     />
-                    
-                    <InputBox 
-                        inputLabel="Password"
-                        inputType="password"
-                        inputPlaceholder="********"
-                    />
-
-                    <Button
-                        buttonType="primary"
-                        className="my-2"
-                    >
-                        Login
-                    </Button>
-
-                    <hr className="my-2"/>
-
-                    <Socials />
-
-                </CardWrapper>
-                </LoadFramerMotion>
+                </div>
+                <div className=" col-span-3 md:col-span-2">
+                    <LoginForm />
+                </div>
 
             </section>
-
-        </div>
+        </>
     )
 };
 
