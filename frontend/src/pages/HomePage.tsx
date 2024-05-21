5273227c8baa2233e3d6c5663c07cadf972943b2
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-21 21:15:16 +0530

    pages: added homepage and landingpage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,7 @@
-import { NavBar } from "../components/navbar";
-
 const HomePage = () => {
     return (
-        <div className="h-screen bg-white dark:bg-dark-backgroundone">
-            <NavBar />
+        <div>
+            Home page
         </div>
     )
 };
---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/navbar";
+
+const LandingPage = () => {
+    return (
+        <div className="h-screen bg-white dark:bg-dark-backgroundone">
+            <NavBar />
+        </div>
+    )
+};
+
+
+export default LandingPage;
