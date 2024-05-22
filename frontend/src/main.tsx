28e8a82e626c383d15968851cde52cd03a4be997
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-22 14:23:48 +0530

    added packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <BrowserRouter>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <App />
+          <RecoilRoot>
+            <App />
+          </RecoilRoot>
         </GoogleOAuthProvider>
       </BrowserRouter>
   </React.StrictMode>,
