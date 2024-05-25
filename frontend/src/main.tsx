5512c4cdf7e442970964449217d28a206335dee7
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-05-25 16:37:42 +0530

    added: packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,11 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { RecoilRoot } from 'recoil'
+import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CookiesProvider>
       <BrowserRouter>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
           <RecoilRoot>
@@ -15,5 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </RecoilRoot>
         </GoogleOAuthProvider>
       </BrowserRouter>
+      </CookiesProvider>
   </React.StrictMode>,
 )
